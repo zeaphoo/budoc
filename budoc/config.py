bf25dfe75e1d1ecb790d15fb720929341232ed80
Wei Zhuo <wei.zhuo@ihandysoft.com>
2015-12-26 12:24:08 +0800

    add load_config

--- budoc/config.py
+++ budoc/config.py
@@ -1,8 +1,52 @@
 
 import yaml
+import os
 
 def load_config(config_file='budoc.yml'):
-    pass
+    with open(config_file, 'rb') as f:
+        d = yaml.load(f)
+        return ConfigDict(d)
+    return ConfigDict()
 
-class Config(object):
-    pass
+
+class ConfigDict(dict):
+    def __contains__(self, k):
+        try:
+            return dict.__contains__(self, k) or hasattr(self, k)
+        except:
+            return False
+
+    # only called if k not found in normal places
+    def __getattr__(self, k):
+        try:
+            # Throws exception if not in prototype chain
+            return object.__getattribute__(self, k)
+        except AttributeError:
+            try:
+                return self[k]
+            except KeyError:
+                raise AttributeError(k)
+
+    def __setattr__(self, k, v):
+        try:
+            # Throws exception if not in prototype chain
+            object.__getattribute__(self, k)
+        except AttributeError:
+            try:
+                self[k] = v
+            except:
+                raise AttributeError(k)
+        else:
+            object.__setattr__(self, k, v)
+
+    def __delattr__(self, k):
+        try:
+            # Throws exception if not in prototype chain
+            object.__getattribute__(self, k)
+        except AttributeError:
+            try:
+                del self[k]
+            except KeyError:
+                raise AttributeError(k)
+        else:
+            object.__delattr__(self, k)
